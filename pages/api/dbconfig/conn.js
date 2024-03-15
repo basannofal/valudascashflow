d5e00b1e6bd0d5eeb536a9aa4348bec009778f93
basannofal <nofalbasan@gmail.com>
2024-03-15 11:35:23 +0530

    db change

--- pages/api/dbconfig/conn.js
+++ pages/api/dbconfig/conn.js
@@ -9,11 +9,17 @@ const pool = mysql.createPool({
   // waitForConnections: true,
   // max_connections: 150, // Set the maximum number of connections
 
-  host: "aufcart.com",
+  // host: "aufcart.com",
+  // // port: '3306', // Typically 3306
+  // user: "valudaaa_cash_flow",
+  // password: "iXR%suDt).BI",
+  // database: "valudaaa_cash_flow",
+
+  host: "codinghelps.com",
   // port: '3306', // Typically 3306
-  user: "valudaaa_cash_flow",
-  password: "iXR%suDt).BI",
-  database: "valudaaa_cash_flow",
+  user: "codinghelps_cash_flow",
+  password: "isd)t-HEq&E!",
+  database: "codinghelps_cash_flow",
 });
 const conn = pool.promise();
 
